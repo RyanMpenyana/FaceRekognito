86fe502f7291f2381c34fa243ec975e1212985db
unknown <ivin@tenxrs.com>
2024-02-26 20:39:37 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navigation from "./components/Navigation";
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
+    </>
+  );
+};
+export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,13 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <p
+        style={{ display: "flex", justifyContent: "flex-end" }}
+        className="f3 link dim black underline pa3 pointer "
+      >
+        Sign-out
+      </p>
+    </nav>
+  );
+};
+export default Navigation;
